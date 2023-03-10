16311e7abada920ae23cb089199ffa78036d3c21
ozenilsonalisson <ozenilsonalisson@gmail.com>
2023-03-09 22:48:19 -0300

    Atualizacoes na atividade 5

--- atividade5/domParser.py
+++ atividade5/domParser.py
@@ -2,29 +2,31 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 
+pagina = open('index.html', 'w', encoding='UTF-8')
+pagina.write("<!DOCTYPE html>\n")
+pagina.write("<html lang='en'>\n")
+pagina.write("<head>\n")
+pagina.write("  <meta charset='UTF-8' />\n")
+pagina.write("  <meta http-equiv='X-UA-Compatible' content='IE=edge' />\n")
+pagina.write("  <meta name='viewport' content='width=device-width, initial-scale=1.0' />\n")
+pagina.write("  <title>Atividade 5</title>\n")
+pagina.write("</head>\n")
+pagina.write("<body>\n")
+
 with open('seeds.txt', 'r', encoding='UTF-8') as arquivo:
     seeds = arquivo.readlines()    
     
-    
-print("<!DOCTYPE html>")
-print("<html lang='en'>")
-print("<head>")
-print("<meta charset='UTF-8' />")
-print(" <meta http-equiv='X-UA-Compatible' content='IE=edge' />")
-print("<meta name='viewport' content='width=device-width, initial-scale=1.0' />")
-print("<title>Atividade 5</title>")
-print("</head>")
-print("<body>")
-
 for link in seeds:
     page = urllib.request.urlopen(link)
     soup = BeautifulSoup(str(page.read().decode('utf-8')))
 
-    print("<p>"+soup.title.string+"</p>")
+    pagina.write("  <p>"+soup.title.string+"</p>\n")
     for img in soup.find_all('img'):
         img = img.attrs.get("src")
         if "https://" in img:
-            print("<img width='500px' src='" + img + "'/>")
+            pagina.write("  <img width='500px' src='" + img + "'/>\n\n")
         else:
-            print(f"<img width='500px' src='{link[:-1]+img}'/>")
+            pagina.write(f"  <img width='500px' src='{link[:-1]+img}'/>\n\n")
+pagina.write("</body>\n")
+pagina.write("</html>\n")
         
